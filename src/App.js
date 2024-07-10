1e4e9aa69f0365a6c2abd2b8ba56f1082ecdd1a6
Deva9754 <deva.sahu2001@gmail.com>
2024-07-10 12:24:27 +0530

    install d3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import CommonElements from "./components/commonElements";
+import DataBinding from "./components/DataBinding";
+import SelectElement from "./components/SelectElement";
 
 function App() {
   return (
     <div className="App">
       <CommonElements />
+      <SelectElement />
+      <DataBinding />
     </div>
   );
 }
